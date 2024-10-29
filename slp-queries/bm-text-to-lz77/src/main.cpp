13e9a38688d76325d4b02bf6fc851db9765dabf0
Ankith <ankithreddy2001@gmail.com>
2024-10-28 20:25:49 -0400

    Adj. List Integration

--- slp-queries/bm-text-to-lz77/src/main.cpp
+++ slp-queries/bm-text-to-lz77/src/main.cpp
@@ -22,9 +22,10 @@ std::uint64_t get_hash(const uint64_t &x) {
 struct Block {
    uint64_t length;
    uint64_t position;
+   uint64_t next;
 
    Block(uint64_t position, uint64_t length) : length(length), position(position) {
-    
+    next = -1;
    }
 
    Block() {
@@ -33,9 +34,26 @@ struct Block {
    }
 };
 
+struct ListPtr {
+    // Index of first element.
+    uint64_t head;
+    // Index of last element.
+    uint64_t tail;
+
+    ListPtr(uint64_t head, uint64_t tail) : head(head), tail(tail) {
+
+    }
+
+    ListPtr() {
+        head = -1;
+        tail = -1;
+    }
+};
+
 class BM {
 private:
-    hash_table<uint64_t, Block> hash_block;
+    hash_table<uint64_t, ListPtr> hash_block;
+    space_efficient_vector<Block> blocks;
    
     const char* text;
     uint64_t block_size;
@@ -92,9 +110,17 @@ public:
             }
 
             if(position % block_size == 0) {
+                const Block new_block = Block(position, block_size);
                 if(hash_block.find(current_hash) == NULL) {
-                    const Block new_block = Block(position, block_size);
-                    hash_block.insert(current_hash, new_block);
+                    const ListPtr list_ptr = ListPtr(blocks.size(), blocks.size());
+                    blocks.push_back(new_block);
+                    hash_block.insert(current_hash, list_ptr);
+                }
+                else {
+                    ListPtr* list_ptr = hash_block.find(current_hash);
+                    blocks[list_ptr->tail].next = blocks.size();
+                    list_ptr->tail = blocks.size();
+                    blocks.push_back(new_block);
                 }
             }
 
@@ -102,54 +128,93 @@ public:
             uint64_t begin_len = 0;
             uint64_t end_len = 0;
             {
-                
-                const Block* block = hash_block.find(current_hash);
-                // const Block* block = hash_block[current_hash];
-                if(block != NULL && block->position != position) {
-                    // Note the difference
-                    uint64_t block_end_position = block->position + block->length;
-                    uint64_t current_end_position = position + block_size;
+                const ListPtr* list_ptr = hash_block.find(current_hash);
 
-                    uint64_t block_begin_position = block->position - 1;
-                    uint64_t current_begin_position = position - 1;
-
-                    // Forward Match.
-                    uint64_t end_index = 0;
-
-                    while(block_end_position + end_index < text_length && current_end_position + end_index < text_length) {
-                        if(text[block_end_position + end_index] != text[current_end_position + end_index]) {
+                if(list_ptr != NULL) {
+                    uint64_t list_start_index = list_ptr->head;
+                    // cout << position << ' ' << list_start_index << endl;
+                    while(list_start_index != -1) {
+                        const Block* block = &blocks[list_start_index];
+                        // cout << block->position << endl;
+                        if(block->position >= position) {
                             break;
                         }
 
-                        end_index++;
-                    }
+                        bool stringsEqual = true;
 
-                    
-
-                    // Moved end_index steps forward.
-
-                    // Backward Match -- Max block_size - 1 characters
-
-                    uint64_t begin_index = 0;
-                    // uint64_t parsingIndex = parsing.size(); 
-
-                    while(block_begin_position - begin_index >= 0 && current_begin_position - begin_index >= 0 && begin_index < block_size - 1
-                    && parsing.size() > 0 && parsing.back().second == 0) {
-                        if(text[block_begin_position - begin_index] != text[current_begin_position - begin_index]) {
-                            break;
+                        uint64_t block_start = block->position;
+                        uint64_t curr_start = position;
+                        uint64_t len = 0;
+                        while(len < block_size) {
+                            if(text[block_start + len] != text[curr_start + len]) {
+                                stringsEqual = false;
+                                break;
+                            }
+                            len++;
                         }
-                        parsing.pop_back();
-                        begin_index++;
+
+                        if(!stringsEqual) {
+                            list_start_index = block->next;
+                            continue;
+                        }
+
+                        // cout << "Pos: " << position << ' ' << " | StringsEqual: " << stringsEqual << endl;
+
+                        // const Block* block = hash_block[current_hash];
+                        if(block != NULL && block->position != position) {
+                            // Note the difference
+                            uint64_t block_end_position = block->position + block->length;
+                            uint64_t current_end_position = position + block_size;
+
+                            uint64_t block_begin_position = block->position - 1;
+                            uint64_t current_begin_position = position - 1;
+
+                            // Forward Match.
+                            uint64_t end_index = 0;
+
+                            while(block_end_position + end_index < text_length && current_end_position + end_index < text_length) {
+                                if(text[block_end_position + end_index] != text[current_end_position + end_index]) {
+                                    break;
+                                }
+
+                                end_index++;
+                            }
+
+                            
+
+                            // Moved end_index steps forward.
+
+                            // Backward Match -- Max block_size - 1 characters
+
+                            uint64_t begin_index = 0;
+                            // uint64_t parsingIndex = parsing.size(); 
+
+                            while(block_begin_position - begin_index >= 0 && current_begin_position - begin_index >= 0 && begin_index < block_size - 1
+                            && parsing.size() > 0 && parsing.back().second == 0) {
+                                if(text[block_begin_position - begin_index] != text[current_begin_position - begin_index]) {
+                                    break;
+                                }
+                                parsing.pop_back();
+                                begin_index++;
+                            }
+
+                            begin_len = begin_index;
+                            end_len = end_index;
+
+                            match.position = block->position - begin_index;
+                            match.length = block->length + begin_index + end_index;
+                        }
+
+                        list_start_index = block->next;
+
+                        // cout << "Pos: " << position << " | lis_start_index: " << list_start_index << endl;
                     }
-
-                    begin_len = begin_index;
-                    end_len = end_index;
-
-                    match.position = block->position - begin_index;
-                    match.length = block->length + begin_index + end_index;
                 }
+
+                
             }
 
+            // cout << "match.length: " << match.length << endl;
             if(match.length > 0) {
                 parsing.push_back(make_pair(match.position, match.length));
                 // cout << "Line 151" <<  position << ' ' << match.position << ' ' << match.length << endl;
@@ -162,6 +227,8 @@ public:
                 // cout << position << ' ' << position << ' ' << 0 << endl;
                 ++position;
             }
+
+            // cout << "position: " << position << endl;
 
             // cout << position << endl;
         }
@@ -213,7 +280,7 @@ int main() {
             Extend backward only if there is a alone character?
         2. zxQPYxQYPzab block_size = 2
     */
-    const char* sample_text = "acxybcbcxy";
+    const char* sample_text = "zxQPYxQYPzab";
     uint64_t block_size = 2;
     BM bm(sample_text, block_size);
 
